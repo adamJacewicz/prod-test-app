e4b705afadffe28c56ce79eb937ebaa01de8a3a1
adam <adam.jacewicz4@gmail.com>
2024-08-07 22:46:55 +0200

    fix: e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,10 +9,13 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+console.log(devices);
 export default defineConfig({
   testDir: './tests/e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
+  timeout: 10000,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
