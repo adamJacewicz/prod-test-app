a343b021c1b3af261328d98ca3bf01baa62af54e
adam <adam.jacewicz4@gmail.com>
2024-08-18 22:14:58 +0200

    feat: error-metrics

--- terraform/modules/error/cloudwatch.tf
+++ terraform/modules/error/cloudwatch.tf
@@ -4,43 +4,21 @@ resource "aws_cloudwatch_dashboard" "frd_error_dashboard" {
   dashboard_body = jsonencode({
     widgets = [
       {
-        type   = "metric"
-        x      = 0
-        y      = 0
-        width  = 12
-        height = 6
-
-        properties = {
-          metrics = [
-            [
-              "RMCatalogMetrics", "FrontendErrors"
-            ]
-          ]
-          period = 86400 * 7
-          stat   = "p95"
-          view   = "singleValue"
-          region = "eu-central-1"
-          title  = "Frontend Errors Over Time"
+        type : "metric",
+        x : 0,
+        y : 0,
+        width : 24,
+        height : 6,
+        properties : {
+          metrics : [
+            [ "RMCatalogMetrics", "FrontendErrors" ]
+          ],
+          period : 300,
+          stat : "Sum",
+          region : "eu-central-1",
+          title : "Sum of Frontend Errors"
         }
-      },
-      {
-        type   = "metric"
-        x      = 12
-        y      = 0
-        width  = 12
-        height = 6
-
-        properties = {
-          metrics = [
-            [
-              "RMCatalogMetrics", "FrontendErrors"
-            ]
-          ]
-          view    = "timeSeries"
-          stacked = false,
-          region  = "eu-central-1"
-          title   = "Total Error Count (Last 10 Minutes)"
-        }
+      }
       }
     ]
   })
