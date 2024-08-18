9006516fd9e73af55c385325e21c97edb6f74925
Adam <35642115+adamJacewicz@users.noreply.github.com>
2024-08-18 22:22:07 +0200

    Error metrics (#34)
    * feat: error-metrics

--- terraform/state-resources.tf
+++ terraform/state-resources.tf
@@ -25,9 +25,9 @@ resource "aws_s3_bucket_acl" "terraform_state_bucket_acl" {
 }
 
 resource "aws_dynamodb_table" "state_lock_table" {
-  billing_mode                = "PAY_PER_REQUEST"
-  hash_key                    = "LockID"
-  name                        = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  name         = "terraform-state-lock"
   attribute {
     name = "LockID"
     type = "S"
